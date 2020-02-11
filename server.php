999e99bb5468723a24dabe744b8171a0c03186c8
anazawa yasuhiro <yasuhiro@humming-code.com>
2020-02-11 11:42:58 +0900

    ReactPHPバージョンアップ

--- server.php
+++ server.php
@@ -1,34 +1,26 @@
 <?php
 
-use Olt\App;
+use olt\App;
 
 require "vendor/autoload.php";
 
-spl_autoload_register(function($class) {
-    include 'src/' . strtr($class, "\\", DIRECTORY_SEPARATOR) . '.php';
-});
-
 $loop = React\EventLoop\Factory::create();
-$socket = new React\Socket\Server($loop);
+$socket = new React\Socket\Server("0.0.0.0:23", $loop);
 $clients = [];
 
-$socket->on('connection', function ($conn) use(&$clients) {
-
-    $now = new DateTime();
-    echo $now->format('c') . ": " . $conn->getRemoteAddress() . PHP_EOL;
+$socket->on('connection', function (React\Socket\ConnectionInterface $connection) use(&$clients) {
+    echo (new \DateTimeImmutable())->format('c') . ": " . $connection->getRemoteAddress() . PHP_EOL;
     if (count($clients) > 600) {
-        $conn->end("サーバーが混雑している為接続できません。\r\n");
+        $connection->end("サーバーが混雑している為接続できません。\r\n");
     }
 
-    $id = 0;
     do {
        $id = mt_rand(1, 999);
     } while(in_array($id, $clients));
 
     $app = new App();
-    $app->connect($id, $conn, $clients);
+    $app->connect($id, $connection, $clients);
     $clients[spl_object_hash($app)] = $id;
 });
 
-$socket->listen(23, "0.0.0.0");
 $loop->run();
